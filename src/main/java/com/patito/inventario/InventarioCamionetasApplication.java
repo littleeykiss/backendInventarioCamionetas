e6b27169dd2770801f5c28a24ca3e911f76ac7b9
Erik Sanchez <erik.sanchez@sofipa.org.mx>
2025-07-18 13:35:06 -0600

    Commit inicial del backend Inventario

---
+++ src/main/java/com/patito/inventario/InventarioCamionetasApplication.java
@@ -0,0 +1,13 @@
+package com.patito.inventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioCamionetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioCamionetasApplication.class, args);
+	}
+
+}
